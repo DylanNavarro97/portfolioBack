a656bb739523560261a4801631e6f6d50902e86d
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-05-15 16:44:18 -0300

    quite mensaje de validacion nodemailer y tambien cambie el puerto a seguro

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -3,14 +3,10 @@ import 'dotenv/config'
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.mail,
       pass: process.env.adminAPIPass,
     },
   });
-
-  transporter.verify().then(() => {
-    console.log("ready for send emails")
-  })
