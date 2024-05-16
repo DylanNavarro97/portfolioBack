ff28173e641dbc87a1b82e62181559acb2689e2c
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-05-16 00:42:36 -0300

    correcciones de length a remitente

--- src/helpers/validacionMail.js
+++ src/helpers/validacionMail.js
@@ -14,13 +14,18 @@ const validacionMail = [
     .notEmpty()
     .withMessage("El mail del remitente es un dato obligatorio.")
     .isEmail()
-    .withMessage("El remitente debe ser una dirección de correo electrónico válida."),
+    .withMessage("El remitente debe ser una dirección de correo electrónico válida.")
+    .isLength({
+        min: 2,
+        max: 50,
+    })
+    .withMessage("El mail del remitente debe estar entre 2 y 80 caracteres"),
     check("asunto")
     .notEmpty()
     .withMessage("El asunto es un dato obligatorio.")
     .isLength({
         min: 2,
-        max: 100
+        max: 50
     })
     .withMessage("El asunto debe contener entre 2 y 100 caracteres."),
     check("mensaje")
